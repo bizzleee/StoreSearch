2ceee2ac854f550b677cb2a7f10b24a9cd6f6718
Alejandro Pallares <bizzlee200@gmail.com>
2022-07-10 20:38:49 -0500

    Initial Commit

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Alejandro Pallares on 09/07/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
